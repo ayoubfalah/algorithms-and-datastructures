f88e86582b729c20e685fbb7c68878eff2abc928
Ayoub Falah <ayoub_falah@hotmail.de>
2019-09-28 18:33:08 +0200

    Searching a list of strings using binary search

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -14,10 +14,21 @@ public class SearchEngine{
 			return NOT_IN_LIST;
 
 		}
+	private static Interval binarySearch(List<String> list, String key){
+		int d = -1;
+		int f = list.size();
+		while(d+1 < f){
+			int i = (d+f)/2;
+			if(key.compareTo(list.get(i)) == 0) return new Interval(i,i);
+			else if(key.compareTo(list.get(i)) > 0) d = i;
+			else f = i;
+			}
+		return new Interval(d,f);
+		}
 	public static void main(String args[]){
 		List list = Arrays.asList("a", "b", "aa", "ab", "bb", "ba");
 		String key = "ab";
-		int index = contains(list, key);
-		System.out.println(index);
+		Interval location = binarySearch(list, key);
+		System.out.println(location);
 		}
 	}
