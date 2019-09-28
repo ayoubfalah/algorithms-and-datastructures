e6438f9846b9e9ab80122deadfb2ec3187c8df4a
Ayoub Falah <ayoub_falah@hotmail.de>
2019-09-28 18:34:45 +0200

    Representing an interval with natural numbers

---
+++ src/Interval.java
@@ -0,0 +1,14 @@
+public class Interval{
+
+	private int a;
+	private int b;
+	
+	public Interval(int a, int b){
+		this.a = a;
+		this.b = b;
+		}
+	
+	public String toString(){
+		return "[" + a + "," + b + "]";
+		}
+		}
